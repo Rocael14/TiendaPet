fcbcbae5b51324a08ceedd58b6c9a4805d495d8e
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-08-20 16:41:48 -0600

    Se comienza con mi primera clase

---
+++ main.py
@@ -0,0 +1,7 @@
+class Producto:
+    def __init__(self, nombre, id_categoria, precio, total_compras, total_ventas):
+        self.nombre = nombre
+        self.id_categoria = id_categoria
+        self.precio = precio
+        self.total_compras = total_compras
+        self.total_ventas = total_ventas
