84da3b90793f198e7fea81ac5766782439f21e4f
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-08-22 00:07:34 -0600

    Se crean mas clases y una clase para calcular el stock

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 class Producto:
-    def __init__(self, nombre, id_categoria, precio, total_compras, total_ventas):
+    def __init__(self, id_producto,nombre, id_categoria, precio, total_compras, total_ventas):
+        self.id_producto = id_producto
         self.nombre = nombre
         self.id_categoria = id_categoria
         self.precio = precio
         self.total_compras = total_compras
         self.total_ventas = total_ventas
+        self.stock = CalcularStock.calcular(total_compras, total_ventas)
+
+class CalcularStock:
+    def calcular(self, total_compras, total_ventas):
+        return total_compras-total_ventas
+class Categoria:
+    def __init__(self, id_categoria, nombre):
+        self.id_categoria = id_categoria
+        self.nombre = nombre
+
+class Clientes:
+    def __init__(self, id_cliente, nombre, telefono, direccion, correo):
+        self.id_cliente = id_cliente
+        self.nombre = nombre
+        self.telefono = telefono
+        self.direccion = direccion
+        self.correo = correo
+
+class Empleados:
+    def __init__(self, id_empleado, id_tipo_empleado,nombre, telefono, direccion, correo):
+        self.id_empleado = id_empleado
+        self.id_tipo_empleado = id_tipo_empleado
+        self.nombre = nombre
+        self.telefono = telefono
+        self.direccion = direccion
+        self.correo = correo
+
+class TipoEmpleados:
+    def __init__(self, id_tipo_empleado, nombre):
+        self.id_tipo_empleado = id_tipo_empleado
+        self.nombre = nombre
+
